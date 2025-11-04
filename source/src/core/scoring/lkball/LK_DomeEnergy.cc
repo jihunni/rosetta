94d7c00e7af3e9441aadd8088f5a04a71a382baf
Jihun <jeung@uw.edu>
2025-11-04 07:51:09 -0800

    compiler error fix

--- source/src/core/scoring/lkball/LK_DomeEnergy.cc
+++ source/src/core/scoring/lkball/LK_DomeEnergy.cc
@@ -1034,15 +1034,13 @@ cubed_root( Real val ) {
 
 
 DerivativeFinderWadj::DerivativeFinderWadj( Vector const & base, Vector const & water, Real w_dist, Real water_adjust )
-: x(14)
-{
-
-	w_dist_ = w_dist;
-	wadj = water_adjust;
-
-	B = base;
-	W = water;
-	B_to_W = W - B;
+	: B( base ),
+	W( water ),
+	B_to_W( water - base ),
+	wadj( water_adjust ),
+	w_dist_( w_dist ),
+	x(14)
+    {
 	B_to_W_norm = B_to_W.norm();
 	B_to_W_norm2 = B_to_W_norm*B_to_W_norm;
 	B_to_W_norm3 = B_to_W_norm2*B_to_W_norm;
