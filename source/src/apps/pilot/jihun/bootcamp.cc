18245a7aec37f2a5701ce48f052a8e591a6c896c
Jihun <jeung@uw.edu>
2025-11-07 12:31:45 -0800

    Revert "pose and scorefunction" Remove src/apps/pilot/jihun/bootcamp.cc
    This reverts commit 50706d2d43a44d56025a1ba3c232598847884760.

--- source/src/apps/pilot/jihun/bootcamp.cc
+++ source/src/apps/pilot/jihun/bootcamp.cc
@@ -8,33 +8,9 @@
 // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
 #include <iostream>
-#include <basic/options/option.hh>
-#include <basic/options/keys/in.OptionKeys.gen.hh>
-#include <devel/init.hh>
-#include <utility/pointer/owning_ptr.hh>
-#include <core/pose/Pose.hh>
-#include <core/import_pose/import_pose.hh>
-#include <core/scoring/ScoreFunctionFactory.hh>
-#include <core/scoring/ScoreFunction.hh>
 
-int main( int argc, char ** argv) {
-	std::cout << "Hello World!" << std::endl;
-
-	devel::init( argc, argv );
-	utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
-	if ( filenames.size() > 0 ) {
-	std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
-	} else {
-		std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
-		return 1;
-	}
-	
-	core::pose::PoseOP pose = core::import_pose::pose_from_file( filenames[1] );
-	//core::pose::PoseOP pose = core::import_pose::import_pose::pose_from_file( pdb )
-	core::scoring::ScoreFunctionOP sfxn = core::scoring::get_score_function() ; 
-	core::Real score = sfxn->score( *pose );
-	std::cout << "Score: " << score << std::endl ;
-
+int main() {
+	std::cout << "Hello World!!" << std::endl;
 	return 0;
 } 
 
